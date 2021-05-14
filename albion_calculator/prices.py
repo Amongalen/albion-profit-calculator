60ebf6f5709cdf66021c12aef539739b2ad79d37
Amongalen <1gumis@gmail.com>
2021-05-14 16:37:27 +0200

    getting prices from API

--- albion_calculator/prices.py
+++ albion_calculator/prices.py
@@ -1,4 +1,5 @@
 import functools
+import json
 import pathlib
 from datetime import datetime, timedelta
 
@@ -9,6 +10,78 @@ from albion_calculator import items
 CACHE_LIFETIME = 12
 
 CACHE_FILENAME = '../cache/price.cache'
+
+API_ADDRESS = 'https://www.albion-online-data.com/api/v2/stats/{type}/{items}.json'
+
+CITIES = ['Bridgewatch', 'Fort Sterling', 'Caerleon', 'Martlock', 'Thetford', 'Lymhurst']
+REQUEST_PARAMS = {'locations': ','.join(CITIES),
+                  'time-scale': 1,
+                  'qualities': 1}
+
+
+def local_price_cache(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        result = load_local_cache()
+        if result is None:
+            result = func(*args, **kwargs)
+            write_to_local_cache(result)
+        return result
+
+    return wrapper
+
+
+@local_price_cache
+def get_all_prices(items):
+    all_prices = {}
+    for item in items:
+        prices = get_prices_for_one_item(item)
+        all_prices[item] = prices
+    return all_prices
+
+
+def get_prices_for_one_item(item):
+    history_url = API_ADDRESS.format(type='history', items=item)
+    prices_url = API_ADDRESS.format(type='prices', items=item)
+
+    history_prices = get_json_from_url(history_url)
+    latest_prices = get_json_from_url(prices_url)
+    history_prices_by_city = {record['location']: record for record in history_prices}
+    latest_prices_by_city = {record['city']: record for record in latest_prices}
+    merged_prices_by_city = {}
+    for city in CITIES:
+        history_price = history_prices_by_city.get(city, {})
+        history_price_summary = summarize_history_price(history_price)
+        latest_price = latest_prices_by_city.get(city, {})
+        merged_prices_by_city[city] = history_price_summary | latest_price
+    return merged_prices_by_city
+
+
+def summarize_history_price(history_price):
+    summary = {}
+    return summary
+
+
+def get_json_from_url(url):
+    response = requests.get(url, params=REQUEST_PARAMS)
+    if not response:
+        response_json = None
+    else:
+        response_json = response.json()
+    return response_json
+
+
+def load_local_cache():
+    if not is_cache_up_to_date():
+        return None
+    with open(CACHE_FILENAME) as f:
+        cache = json.load(f)
+    return cache
+
+
+def write_to_local_cache(cache_json):
+    with open(CACHE_FILENAME, 'w') as f:
+        json.dump(cache_json, f, indent=1)
 
 
 def is_cache_up_to_date():
@@ -27,37 +100,10 @@ def is_cache_up_to_date():
     return True
 
 
-def load_local_cache():
-    if not is_cache_up_to_date():
-        return None
-    with open(CACHE_FILENAME) as f:
-        cache = f.read()
-    return cache
-
-
-def write_to_local_cache(cache_text):
-    with open(CACHE_FILENAME, 'w') as f:
-        f.write(cache_text)
-
-
-def local_price_cache(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        result = load_local_cache()
-        if result is None:
-            result = func(*args, **kwargs)
-            write_to_local_cache(result)
-        return result
-
-    return wrapper
-
-
-@local_price_cache
-def get_prices(items):
-    return 'test5'
-
-
 if __name__ == '__main__':
-    items = items.load_items()
-    result = get_prices(items)
+    # items = items.load_items()
+    # items_ids = items.keys()
+
+    items_ids = ['T5_WOOD', 'T5_PLANKS', 'T5_BAG', 'T4_POTION_HEAL']
+    result = get_all_prices(items_ids)
     print(result)
